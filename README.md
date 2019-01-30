b84b8c13b04f169660a98d79a4060ffee2481c68
jtshannon <jtshannon@gmail.com>
2019-01-30 00:41:57 -0500

    End of part 2

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-#Godot tutorial
-Project from Godot tutorial by fornclake
+# zelda_demo
+
+Project from Godot YouTube tutorial by fornclake
+
 https://www.youtube.com/watch?v=M4wuhFgubWY&list=PLv0bAfkzWSiY4d_FJlQTlFOZh34nrlJZY&index=2
--- player/player.gd
+++ player/player.gd
@@ -2,23 +2,54 @@ extends KinematicBody2D
 
 const SPEED = 70
 
-var movedir = Vector2(0,0)
+var moveDir = Vector2(0,0)
+var spriteDir = "down"
 
 func _physics_process(delta):
 	controls_loop()
 	movement_loop()
+	spriteDir_loop()
 	
+	if is_on_wall():
+		if spriteDir == "Left" and test_move(transform, Vector2(-1,0)):
+			anim_switch("push")
+		if spriteDir == "Right" and test_move(transform, Vector2(1,0)):
+			anim_switch("push")
+		if spriteDir == "Up" and test_move(transform, Vector2(0,-1)):
+			anim_switch("push")
+		if spriteDir == "Down" and test_move(transform, Vector2(0,1)):
+			anim_switch("push")
+	elif moveDir != Vector2(0,0):
+		anim_switch("walk")
+	else:
+		anim_switch("idle")
+
 func controls_loop():
 	var LEFT = Input.is_action_pressed("ui_left")
 	var RIGHT = Input.is_action_pressed("ui_right")
 	var UP = Input.is_action_pressed("ui_up")
 	var DOWN = Input.is_action_pressed("ui_down")
 	
-	movedir.x = -int(LEFT) + int(RIGHT)
-	movedir.y = -int(UP) + int(DOWN)
+	moveDir.x = -int(LEFT) + int(RIGHT)
+	moveDir.y = -int(UP) + int(DOWN)
 	
 func movement_loop():
-	var motion = movedir.normalized() * SPEED
+	var motion = moveDir.normalized() * SPEED
 	move_and_slide(motion, Vector2(0,0))
 	
+func spriteDir_loop():
+	match moveDir:
+		Vector2(-1,0):
+			spriteDir = "Left"
+		Vector2(1,0):
+			spriteDir = "Right"
+		Vector2(0,-1):
+			spriteDir = "Up"
+		Vector2(0,1):
+			spriteDir = "Down"
+			
+func anim_switch(animation):
+	var newAnim = str(animation, spriteDir)
 	
+	if $Anim.current_animation != newAnim:
+		$Anim.play(newAnim)
